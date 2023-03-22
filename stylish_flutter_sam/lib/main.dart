63426a3c6de773bd7113fee9d689f743d59985e8
“Sam <s00001sam@gmail.com>
2023-03-23 00:43:54 +0800

    stylish - complete home top banner

--- stylish_flutter_sam/lib/main.dart
+++ stylish_flutter_sam/lib/main.dart
@@ -10,7 +10,73 @@ class StylishApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/ic_stylish_logo.png",
+          fit: BoxFit.contain,
+          width: 130,
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xffe1e1e1),
+      ),
+      body: Column(
+        children: const [
+          HomeBanner(),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeBanner extends StatelessWidget {
+  const HomeBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      height: 200,
+      child: _horizontalList(10),
+    );
+  }
+
+  ListView _horizontalList(int n) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: List.generate(n, (i) => const BannerCard()),
+    );
+  }
+}
+
+class BannerCard extends StatelessWidget {
+  const BannerCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: Card(
+        semanticContainer: true,
+        margin: const EdgeInsets.all(8.0),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        child: Image.asset(
+          "assets/images/cloth01.jpg",
+          fit: BoxFit.fill,
+        ),
+      ),
     );
   }
 }
