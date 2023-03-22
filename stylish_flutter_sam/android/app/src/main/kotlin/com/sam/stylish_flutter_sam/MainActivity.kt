d4afe52f88b4622a174730a11ec203e0f4560a8d
“Sam <s00001sam@gmail.com>
2023-03-23 00:41:12 +0800

    init project

---
+++ stylish_flutter_sam/android/app/src/main/kotlin/com/sam/stylish_flutter_sam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sam.stylish_flutter_sam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ stylish_flutter_sam/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StylishApp());
+}
+
+class StylishApp extends StatelessWidget {
+  const StylishApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(),
+    );
+  }
+}
